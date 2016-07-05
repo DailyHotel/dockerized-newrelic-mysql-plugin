52a75b898e137277983a43884b127611f2e47143
Keaton Choi <keaton@dailyhotel.com>
2016-07-05 22:24:15 +0900

    Add Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+cd /root/newrelic-npi
+
+./npi config set license_key ${LICENSE_KEY} && ./npi install nrmysql --user=root --quiet --yes --noedit
+
+if [ -f /data/nrmysql.plugin.json ];
+then
+ cp -f /data/nrmysql.plugin.json /root/newrelic-npi/plugins/com.newrelic.plugins.mysql.instance/newrelic_mysql_plugin-2.0.0/config/plugin.json
+fi
+
+./npi start nrmysql --foreground
